d23251e6a40b0b6c9b72e39664e94f8cc12cc2c7
Евгений Наконечный <NakonechnyyEvgeniy@outlook.com>
2024-04-19 12:39:38 +0300

    Init

---
+++ Webview2WinformsApp/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Webview2WinformsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView2
+            // 
+            this.webView2.AllowExternalDrop = true;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Location = new System.Drawing.Point(12, 29);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(1160, 736);
+            this.webView2.TabIndex = 1;
+            this.webView2.ZoomFactor = 1D;
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ShowIcon = false;
+            this.ClientSize = new System.Drawing.Size(1184, 777);
+            this.Controls.Add(this.webView2);
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+    }
+}
+
---
+++ Webview2WinformsApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace Webview2WinformsApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _url = "https://github.com/login";
+
+        public Form1()
+        {
+            InitializeComponent();
+            AttachControlEventHandlers();
+
+            Resize += Form_Resize;
+
+            InitializeAsync();
+        }
+
+        void AttachControlEventHandlers()
+        {
+            webView2.CoreWebView2InitializationCompleted += WebView2Control_CoreWebView2InitializationCompleted;
+            webView2.NavigationStarting += WebView2Control_NavigationStarting;
+            webView2.NavigationCompleted += WebView2Control_NavigationCompleted;
+        }
+
+        private async void InitializeAsync()
+        {
+            Log("EnsureCoreWebView2Async executed.");
+            await webView2.EnsureCoreWebView2Async(null);
+
+            webView2.CoreWebView2.CookieManager.DeleteAllCookies();
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView2.Size = this.ClientSize - new System.Drawing.Size(webView2.Location);
+        }
+
+        private void WebView2Control_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            if (!e.IsSuccess)
+            {
+                Log($"WebView2 creation failed with exception = {e.InitializationException}");
+                Log("CoreWebView2InitializationCompleted failed");
+                return;
+            }
+
+            webView2.CoreWebView2.Navigate(_url);
+
+            Log("CoreWebView2InitializationCompleted succeeded");
+        }
+
+        private void WebView2Control_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            Log("NavigationStarting");
+        }
+
+        private void WebView2Control_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            Log("NavigationCompleted");
+        }
+
+        private void Log(string message)
+        {
+            Debug.WriteLine($"{DateTime.Now} : {message}");
+        } 
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            Resize -= Form_Resize;
+            if (webView2 != null)
+            {
+                webView2.Stop();
+                webView2.Dispose();
+            }
+        }
+    }
+}
